27a27ee82b4e0f28b559b75172eea2a36a7f584c
Hardey <hardeylim@gmail.com>
2025-04-29 13:54:54 +0800

    ci: Add schema sync workflow

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_29_053617) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_29_055425) do
   create_table "people", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "middle_name"
   end
 end
